745c3d449cdb0ce901b65b60ff7572076637b067
Jeb <panganibanjebwilfred@gmail.com>
2022-06-03 22:49:34 +0800

    Feat: Create LoginPage and LoginForm widgets

---
+++ panganiban_project/lib/auth/login_page.dart
@@ -0,0 +1,106 @@
+import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+
+class LoginPage extends StatelessWidget {
+  const LoginPage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: SafeArea(
+        child: Padding(
+          padding: EdgeInsets.all(25.0),
+          child: Align(
+            alignment: Alignment.center,
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: const [
+                Icon(
+                  FontAwesomeIcons.twitter,
+                  color: Colors.white,
+                  size: 28.0,
+                ),
+                SizedBox(height: 20.0),
+                LoginForm()
+              ]
+            )
+          )
+        )
+      )
+    );
+  }
+}
+
+class LoginForm extends StatefulWidget {
+  const LoginForm({Key? key}) : super(key: key);
+
+  @override
+  State<LoginForm> createState() => _LoginFormState();
+}
+
+class _LoginFormState extends State<LoginForm> {
+  late TextEditingController _usernameController = TextEditingController();
+  late TextEditingController _passwordController = TextEditingController();
+
+  InputDecoration textFieldStyle(String label) {
+    return InputDecoration(
+      labelText: label,
+      labelStyle: TextStyle(color: const Color(0xFF425364)),
+      hintStyle: TextStyle(color: const Color(0xFF425364)),
+      focusedBorder: const OutlineInputBorder(
+        borderSide: BorderSide(
+          color: Colors.blue,
+        )
+      ),
+      enabledBorder: const OutlineInputBorder(
+        borderSide: BorderSide(
+          color: const Color(0xFF425364),
+        )
+      )
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    ButtonStyle buttonStyle = ButtonStyle(
+        padding: MaterialStateProperty.all<EdgeInsetsGeometry>(
+            const EdgeInsets.all(12.0)
+        ),
+        shape: MaterialStateProperty.all<RoundedRectangleBorder>(
+            RoundedRectangleBorder(
+              borderRadius: BorderRadius.circular(36.0),
+            )
+        )
+    );
+
+    return Container(
+      child: Column(
+        children: [
+          Text('Login to Tuwitta', style: TextStyle(fontSize: 32.0, fontWeight: FontWeight.bold)),
+          SizedBox(height: 16.0),
+          TextField(
+            controller: _usernameController,
+            decoration: textFieldStyle('Username')
+          ),
+          SizedBox(height: 16.0),
+          TextField(
+            controller: _passwordController,
+            obscureText: true,
+            decoration: textFieldStyle('Password')
+          ),
+          SizedBox(height: 16.0),
+          SizedBox(
+            width: double.infinity,
+            child: ElevatedButton(
+              onPressed: () {},
+              child: const Text('Login', style: TextStyle(fontSize: 16.0)),
+              style: buttonStyle,
+            )
+          )
+        ]
+      )
+    );
+  }
+}
+
