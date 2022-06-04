413ee19debb163fd4e12b32c6b3c4ec2f5fe8dd6
Jeb <panganibanjebwilfred@gmail.com>
2022-06-04 08:34:45 +0800

    Feat: Add setters for username, first name, and last name

--- panganiban_project/lib/utils/secure_storage.dart
+++ panganiban_project/lib/utils/secure_storage.dart
@@ -5,4 +5,13 @@ class SecureStorage {
 
   static Future setToken(String token) async =>
       await _storage.write(key: "jwt", value: token);
+
+  static Future setUsername(String username) async =>
+      await _storage.write(key: "username", value: username);
+
+  static Future setFirstName(String firstName) async =>
+      await _storage.write(key: "firstName", value: firstName);
+
+  static Future setLastName(String lastName) async =>
+      await _storage.write(key: "lastName", value: lastName);
 }
