d2b22b21cfcf5ac149a692fe91477720da567f25
Jeb <panganibanjebwilfred@gmail.com>
2022-06-18 19:06:27 +0800

    Feat: Added profile header

--- panganiban_project/lib/profile/view_profile.dart
+++ panganiban_project/lib/profile/view_profile.dart
@@ -18,13 +18,47 @@ class ViewProfilePage extends StatelessWidget {
       appBar: AppBar(
         backgroundColor: new Color(0xFF15202b)
       ),
-      body: Column(
+      body: ListView(
         children: [
-          Text('$username'),
-          Text('$firstName'),
-          Text('$lastName'),
+          ProfileHeader(username: username, firstName: firstName, lastName: lastName),
+          const Divider(height: 1, thickness: 1, color: Color(0xFF425364))
         ]
       )
+
     );
   }
 }
+
+class ProfileHeader extends StatelessWidget {
+  final String username;
+  final String firstName;
+  final String lastName;
+
+  const ProfileHeader({Key? key, required this.username, required this.firstName, required this.lastName}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+        padding: EdgeInsets.all(15.0),
+        child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              CircleAvatar(
+                radius: 50,
+                backgroundColor: Colors.blue.withOpacity(0.0),
+                child: Image.asset("assets/images/profile_icon.png"),
+              ),
+              Text(
+                '@$username',
+                style: TextStyle(fontSize: 16.0, color: Color(0xFF71767B))
+              ),
+              SizedBox(height: 10.0),
+              Text(
+                '$firstName $lastName',
+                style: TextStyle(fontSize: 20.0, fontWeight: FontWeight.bold)),
+            ]
+        )
+    );
+  }
+}
+
